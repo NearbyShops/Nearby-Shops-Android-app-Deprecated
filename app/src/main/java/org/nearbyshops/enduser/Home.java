3ae9e3eb6c194a37a55c13ab12a977a3b8325771
treeofkarma <sumeet.0587@gmail.com>
2016-03-23 12:35:36 +0530

    Initial commit

---
+++ app/src/main/java/org/nearbyshops/enduser/Home.java
@@ -0,0 +1,21 @@
+package org.nearbyshops.enduser;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
