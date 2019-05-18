41fa1ce01f5251994134613e49e5ad339151a5b4
sumeetmoray <sumeet.0587@gmail.com>
2019-05-18 08:18:24 +0530

    routine project update ....

---
+++ app/src/main/java/org/nearbyshops/enduserappnew/APIKeysExample.java
@@ -0,0 +1,12 @@
+package org.nearbyshops.enduserappnew;
+
+
+
+
+
+public class APIKeysExample {
+
+    public static final String OpenMapTilesGeocoder = "abcde";
+    public static final String styleURLBright = "abcde";
+
+}
