14903df301f6718da3204c356d0bb888559f468b
sumeetmoray <sumeet.0587@gmail.com>
2020-03-07 01:06:12 +0530

    improvements in layouts !

--- app/src/main/java/org/nearbyshops/enduserappnew/Checkout/PlaceOrderActivity.java
+++ app/src/main/java/org/nearbyshops/enduserappnew/Checkout/PlaceOrderActivity.java
@@ -45,7 +45,9 @@ public class PlaceOrderActivity extends AppCompatActivity implements View.OnClic
     CartStatsService cartStatsService;
 
 
+
     
+
     @Inject
     OrderService orderService;
 
