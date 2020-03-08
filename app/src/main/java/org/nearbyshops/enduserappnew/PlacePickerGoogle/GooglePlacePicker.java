f4cb4627f5066b18726faf5a36ef3cd0a841152e
sumeetmoray <sumeet.0587@gmail.com>
2020-03-09 00:58:58 +0530

    in the process of adding a google maps place picker !

---
+++ app/src/main/java/org/nearbyshops/enduserappnew/PlacePickerGoogle/GooglePlacePicker.java
@@ -0,0 +1,51 @@
+//package org.nearbyshops.enduserappnew.PlacePickerGoogle;
+//
+//import android.os.Bundle;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.app.AppCompatDelegate;
+//
+//import org.nearbyshops.enduserappnew.R;
+//
+//
+//public class GooglePlacePicker extends AppCompatActivity {
+//
+//
+//    public static final String TAG_PICKUP = "tag_pickup";
+//    public static final String TAG_DESTINATION = "tag_destination";
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+//
+//
+////        overridePendingTransition(R.anim.enter_from_right,R.anim.exit_to_left);
+//        setContentView(R.layout.activity_select_location);
+//
+//
+////        new MapPickerLocalPickup()
+//
+//
+//
+//
+//        if(savedInstanceState==null)
+//        {
+//
+//            // show pickup
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//                    .replace(R.id.fragment_container,new PlacePickerWithMapFragment(),TAG_PICKUP)
+//                    .commitNow();
+//        }
+//
+//    }
+//
+//
+//
+//
+//
+//
+//
+//
+//}
